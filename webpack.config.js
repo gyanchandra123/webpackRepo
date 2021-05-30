04c90cf7f04c42809e7252b92ea75cb47061e255
gyanchandra moirangthem <gyanchandra moirangthem>
2021-05-30 15:24:51 +0530

    after 5th session: using style-loader& css loader webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,16 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader", //3. Inject styles into DOM using the <style> element , order is important
+          "css-loader", //2. Turns css into common js code
+         /*  "sass-loader" //1. Turns sass into css */
+        ]   
+      }
+    ]
+  }
 };
